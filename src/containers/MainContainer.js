0fbfd147e57638a6fa1af3342eaf7aac820e57fa
paubereng <pauberengueras@gmail.com>
2019-06-04 13:11:18 +0200

    Added new components.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import ShopContainer from './ShopContainer';
+import MainMenu from '../components/MainMenu';
+import Footer from '../components/Footer';
 
 class MainContainer extends Component{
   constructor(props) {
@@ -13,7 +12,9 @@ class MainContainer extends Component{
   render(){
     return (
       <Container>
-        <ShopContainer />
+        <MainMenu />
+        {this.props.children}
+        <Footer />
       </Container>
     );
   };
