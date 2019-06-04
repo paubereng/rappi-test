7be43a2bf56a09b599e3653496e6d4e71bf9f923
paubereng <pauberengueras@gmail.com>
2019-06-04 10:52:51 +0200

    Created category menu, first version.

---
+++ src/components/CategoriesMenu.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {Collapse} from 'react-collapse';
+import categories from '../json/categories.json';
+
+class CategoriesMenu extends Component{
+  constructor(props) {
+   super(props);
+   this.state = {}
+  }
+  handleItemMenu = (name) => {
+    this.setState(prevState => ({
+      [name]: !prevState[name]
+    }));
+  }
+  renderlevel = item => {
+    let isLevelOpen = this.state[item.name] === true;
+
+    return (
+      <div key={item.name}>
+        <div onClick={this.handleItemMenu.bind(this, item.name)}>
+          {/*<i className={`fa fa-arrow-${isLevelOpen ? 'up' : 'down'}`}></i>*/}
+          {`${item.name}`}
+        </div>
+        <Collapse id={item.name} isOpened={isLevelOpen} hasNestedCollapse={true} >
+          {this.hasItemChildren(item) ? item.sublevels.map(level => this.renderlevel(level)) : null}
+        </Collapse>
+      </div>
+    )
+  }
+  hasItemChildren = item => {
+    return item.hasOwnProperty('sublevels');
+  }
+
+  render(){
+    console.log(categories)
+    return (
+      <div className="category-menu">
+        {categories.categories.map((category,i) => {
+          return (
+            <div key={i} style={{borderBottom: '1px solid grey'}}>
+              {this.renderlevel(category)}
+            </div>
+          )
+        })}
+      </div>
+    );
+  };
+};
+
+export default CategoriesMenu;
