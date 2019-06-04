fa1b00c86a055b2d4703c5249e80003de8fb9108
paubereng <pauberengueras@gmail.com>
2019-06-04 13:05:48 +0200

    Created footer.

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="container">
+      <p>2019 <i className="fa fa-copyright"></i> Pau Berengueras. Made in Barcelona with <i className="fa fa-heart"></i></p>
+    </footer>
+  );
+};
+
+export default Footer;
