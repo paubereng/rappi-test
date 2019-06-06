b95feeb15fdf77a9a0b6122c9b48d0c7d895deb0
paubereng <pauberengueras@gmail.com>
2019-06-06 17:24:42 +0200

    Added list and item components in the cart container.

--- src/containers/CartContainer.js
+++ src/containers/CartContainer.js
@@ -1,23 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actionsCreators from '../reducers/CartReducer';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import CartList from '../components/CartList';
 
 class CartContainer extends Component{
-  constructor(props) {
-   super(props);
-   this.state = {}
+  handleRemoveProductToCart = (product) => {
+    this.props.deleteProductToCart(product);
   }
-
+  handleAddProductToCart = (product) => {
+    this.props.addProductToCart(product);
+  }
+  handleRemoveProductItemToCart = (product) => {
+    this.props.deleteItemProductToCart(product);
+  }
   render(){
+    let { cart } = this.props.cart;
+
     return (
       <Container>
         <Row>
-          <h1>Cart</h1>
+          <Col>
+            <h1>My cart</h1>
+            <CartList
+              data={cart}
+              handleRemoveProductToCart={this.handleRemoveProductToCart}
+              handleAddProductToCart={this.handleAddProductToCart}
+              handleRemoveProductItemToCart={this.handleRemoveProductItemToCart}
+            />
+          </Col>
         </Row>
       </Container>
     );
   };
 };
 
-export default CartContainer;
+function mapStateToProps(state) {
+  return {
+    cart: state.cart
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(actionsCreators, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartContainer);
