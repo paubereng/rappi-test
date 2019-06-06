2edbb3ee0e5c57d2015f9f8a97dbab4d7cf34bc0
paubereng <pauberengueras@gmail.com>
2019-06-06 20:07:00 +0200

    Added redux-persist library and implemented his methods to persist products in cart.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { createBrowserHistory } from 'history';
-import store from '../store/store';
+import store, { persistor } from '../store/store';
 import MainContainer from '../containers/MainContainer';
 import ShopContainer from '../containers/ShopContainer';
 import CartContainer from '../containers/CartContainer';
@@ -12,14 +13,16 @@ const history = createBrowserHistory();
 const Routes = () => {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <MainContainer>
-          <div>
-            <Route exact path="/" component={ShopContainer} />
-            <Route path="/cart" component={CartContainer} />
-          </div>
-        </MainContainer>
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router history={history}>
+          <MainContainer>
+            <div>
+              <Route exact path="/" component={ShopContainer} />
+              <Route path="/cart" component={CartContainer} />
+            </div>
+          </MainContainer>
+        </Router>
+      </PersistGate>
     </Provider>
   )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,22 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from '../reducers/index';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+const store = createStore(persistedReducer, /* preloadedState, */ composeEnhancers(
   applyMiddleware(thunk)
  ));
+
+export const persistor = persistStore(store);
 
 export default store;
