f3ae869c6397b27c76b466fc8b59d07bd19eff08
paubereng <pauberengueras@gmail.com>
2019-06-09 01:21:40 +0200

    Displayed units and availability product.

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import cs from 'classnames';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
@@ -9,13 +10,22 @@ class ProductItem extends PureComponent {
     this.props.handleAddProductItem(this.props.data);
   }
   render (){
-    let { name, available, price, image } = this.props.data;
+    let { name, available, price, image, quantity } = this.props.data;
+    let availableClass = cs({
+      'product-item__available': true,
+      'product-item__available--in': available,
+      'product-item__available--out': !available
+    });
     return (
       <li className="product-item">
         <Card className={!available ? 'card--disabled' : null}>
         <Card.Img variant="top" src={image} alt="product" className="product-item__image" />
           <Card.Body>
             <Card.Title>{name}</Card.Title>
+            <div>{`${quantity} units`}</div>
+            <div className={availableClass}>
+              {available ? 'In stock' : 'Out of stock'}
+            </div>
           </Card.Body>
           <Card.Footer>
             <div className="product-item__price">{price}</div>
