344084964d49db8d5197f4ecb9f6422e57f05229
paubereng <pauberengueras@gmail.com>
2019-06-09 12:51:44 +0200

    Created utils files with helpers to work with price.

---
+++ src/utils/utils.js
@@ -0,0 +1,6 @@
+export function stringPriceToNumber(stringPrice) {
+  return parseFloat(stringPrice.slice(1).replace(/,/g, "."))
+}
+export function replaceDot(number, decimals = 3) {
+  return number.toFixed(3).replace(/\./g,",");
+}
